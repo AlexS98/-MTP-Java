96ba63d73670a63c790b8e45a4f57db2693f2ee3
Alexander <sania.sviridenko@gmail.com>
2017-09-16 18:38:59 +0300

    Lab_3

---
+++ Lab_3/src/main/java/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Variant 4
+ * Create an array of numbers that consists of 150 elements. The contents of the array generate with Random#nextInt.
+ * Implement five types of data sorts: bubble sort, cocktail sort, quick sort, cycle sort, odd-even sort;
+ * Use two libraries to sort the data.
+ * Three times perform each of the sorts, including sorts with libraries, with the working time measurement of each of them.
+ * Gather the performance (resources and time) metrics of your sorting and libraries sorting.
+ * The time for working of the each sort is output to the console.
+ */
+
+
+public class Main {
+    public static void main(String args[]){
+
+    }
+}
